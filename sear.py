eaf288637cb07a6fe5b140a89794f88eec1b7269
kiwi <3279416570@qq.com>
2022-12-11 17:42:40 +0800

    Changes

--- sear.py
+++ sear.py
@@ -40,17 +40,17 @@ def SearchEveryUrl(content):  # 更加强大的筛选
 
 def SearchOtherPa(content):
     new_list=[]
-    path = '"//?[A-Za-z0-9-\.]+/[A-Za-z0-9-\.#]+"'  # 一级
-    path2 = '"//?[A-Za-z0-9-\.]+/[A-Za-z0-9-]+/[A-Za-z0-9-\.#]+"'  # 二级
-    path3 = '"//?[A-Za-z0-9-\.]+/[A-Za-z0-9-]+/[A-Za-z0-9-]+/[A-Za-z0-9-\.#]+"'  # 三级
-    path4 = '"//?[A-Za-z0-9-\.]+/[A-Za-z0-9-]+/[A-Za-z0-9-]+/[A-Za-z0-9-]+/[A-Za-z0-9-\.#]+"'  # 四级
-    path5 = '"//?[A-Za-z0-9-\.]+/[A-Za-z0-9-]+/[A-Za-z0-9-]+/[A-Za-z0-9-]+/[A-Za-z0-9-]+/[A-Za-z0-9-\.#]+"'  # 五级
-    path6 = '"//?[A-Za-z0-9-\.]+/[A-Za-z0-9-]+/[A-Za-z0-9-]+/[A-Za-z0-9-]+/[A-Za-z0-9-]+/[A-Za-z0-9-]+/[A-Za-z0-9-\.#]+"'  # 六级
+    path = '"//?[A-Za-z0-9-\.]+/[A-Za-z0-9-\.#\?\=]+"'  # 一级
+    path2 = '"//?[A-Za-z0-9-\.]+/[A-Za-z0-9-]+/[A-Za-z0-9-\.#\?\=]+"'  # 二级
+    path3 = '"//?[A-Za-z0-9-\.]+/[A-Za-z0-9-]+/[A-Za-z0-9-]+/[A-Za-z0-9-\.#\?\=]+"'  # 三级
+    path4 = '"//?[A-Za-z0-9-\.]+/[A-Za-z0-9-]+/[A-Za-z0-9-]+/[A-Za-z0-9-]+/[A-Za-z0-9-\.#\?\=]+"'  # 四级
+    path5 = '"//?[A-Za-z0-9-\.]+/[A-Za-z0-9-]+/[A-Za-z0-9-]+/[A-Za-z0-9-]+/[A-Za-z0-9-]+/[A-Za-z0-9-\.#\?\=]+"'  # 五级
+    path6 = '"//?[A-Za-z0-9-\.]+/[A-Za-z0-9-]+/[A-Za-z0-9-]+/[A-Za-z0-9-]+/[A-Za-z0-9-]+/[A-Za-z0-9-]+/[A-Za-z0-9-\.#\?\=]+"'  # 六级
     list = [path, path2, path3, path4, path5, path6]
     for i in list:
         demo_list = re.findall(i, content)
         new_list.extend(demo_list)
-    return demo_list
+    return new_list
 
 
 def SearchGovern(url):
